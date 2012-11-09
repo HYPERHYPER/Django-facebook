1ed92667ea909acd9535cc7f7b0aad3b3cf7596a
laferrera <jason.laferrera@gmail.com>
2012-11-09 13:47:34 -0500

    changed timeout amount

--- open_facebook/api.py
+++ open_facebook/api.py
@@ -43,7 +43,7 @@ from django_facebook.utils import to_int
 import ssl
 logger = logging.getLogger(__name__)
 
-REQUEST_TIMEOUT = 8
+REQUEST_TIMEOUT = 25
 #two retries was too little, sometimes facebook is a bit flaky
 REQUEST_ATTEMPTS = 3
 
